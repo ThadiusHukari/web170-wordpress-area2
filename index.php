dcb0935c35216b31130ca6bac1616b2b60427349
Thadius Hukari <thadiushukari@yahoo.com>
2017-05-12 00:59:31 -0700

    date, author blog posts

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 <!-- START Article (Content) -->
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); //initiates the loop ?>
-    <h2><a href="<?php the_permalink(); //links to the page or posting ?>">
+    <h2><a href="<?php the_permalink(); //links to the page or posting ?>">    
     <?php the_title(); //gets the page or posting title ?></a></h2>
+
+         <small>Posted on <?php the_time('F jS, Y'); ?> by <?php the_author(); ?> in <?php the_category(', '); ?>.</small>
+
     <?php the_content(''); //gets the page or posting content ?>
     <?php endwhile; endif; //ends the loop ?>
     

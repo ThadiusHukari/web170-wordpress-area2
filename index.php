879ff3fa42a6321d8245ec7fbbcab8d278383284
Thadius Hukari <thadiushukari@yahoo.com>
2017-05-11 23:38:37 -0700

    nav and sidebar nav

--- index.php
+++ index.php
@@ -49,33 +49,13 @@
     <h1>303 N 36th St. Seattle, WA 98103</h1>
     </div>
     
-  <!-- START NAVIGATION (Main)
-  <nav>
-    <ul class="slimmenu">
-<li><a href="woodskys.php">Home</a></li>
-<li><a href="about.php">About</a>
-    <ul>
-        <li><a href="history.php">History</a></li>
-        <li><a href="sponsors.php">Sponsors</a></li>
-    </ul>
-</li>
-<li><a href="events.php">Events</a>
-        <ul>
-        <li><a href="music.php">Music</a></li>
-        <li><a href="games.php">Games</a></li>
-        <li><a href="reservations.php">Reservations</a></li>
-    </ul>
-</li>
-<li><a href="daileyspecials.php">Dailey Specials</a></li>
-<li><a href="photos.php">Photos</a></li>
-<li><a href="contact.php">Contact</a></li>
-    </ul>
-  </nav>
-   END NAVIGATION (Main) -->
+<!-- START NAVIGATION (Main)
     
 <!-- Begin WP Menu -->
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => 'nav', 'items_wrap' => '<ul id="navigation" class="slimmenu %2$s">%3$s</ul>', ) ); ?>
 <!-- End WP Menu -->
+    
+<!-- END NAVIGATION (Main) -->
 
     
   </header>
@@ -94,9 +74,10 @@
     <?php the_title(); //gets the page or posting title ?></a></h2>
     <?php the_content(''); //gets the page or posting content ?>
     <?php endwhile; endif; //ends the loop ?>
+    
   <small>index.php</small>
     
-    <?php if(is_404()) { echo '404 Error. What are you lookin for dood?'; } ?>
+    <?php if(is_404()) { echo '404 Error...what are you lookin for dood?'; } ?>
 </article>
     
 <!-- START Article (Chapter) -->
@@ -105,9 +86,48 @@
 <!-- START Aside -->
 <aside>
     
+<!-- START Sec NAV Sidebar -->
+    <div id="sidebar">
+        <h2><?php
+            
+        if(is_page()) { //if were on a page...
+
+        echo get_the_title($post->post_parent); //get the title of parent page
+    
+        }else{
+            echo 'Blog';
+        }
+            
+        ?>
+        </h2>
+        <ul>
+        <?php
+
+        if(is_page()) { //if were on a page...
+
+            if($post->post_parent) { //if the page has a parent...
+            
+                wp_list_pages(array('title_li' => '', 'child_of' => $post-post_parent, ));  // list the children of said parent
+
+            }else{ //if on the parent page...
+                
+                wp_list_pages(array('title_li' => '', 'child_of' => $post->ID, ));
+            }
+        }else{ //if were not on a page...
+
+            wp_list_categories(array('title_li' => '', )); //list the blog categories with the "Blog" title
+            
+            }
+            ?>
+        </ul>
+    </div>
+<!-- END Sec NAV Sidebar -->
+    
+<!-- Start Calendar -->
     <h3>Upcoming Events</h3>
 <div class='embed-container'><iframe src='https://calendar.google.com/calendar/embed?mode=AGENDA&height=600&wkst=1&bgcolor=%23FFFFFF&src=gu6km764sg0vqr2ug43kkhuq5s%40group.calendar.google.com&color=%236B3304&ctz=America%2FLos_Angeles' style='border-width:0' frameborder='0' scrolling='no'></iframe>
     </div>
+<!-- End Calendar -->
 
     <h3>Recent Posts</h3>
     <!-- SnapWidget Instagram Feed -->
